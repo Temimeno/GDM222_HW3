fe78b0557c0b34ac07b525aae2bf1124306d2cac
Kittipitch_1009 <kittipitch.mold@mail.kmutt.ac.th>
2023-03-28 22:13:51 +0700

    Working on No1 and No3

---
+++ No1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        //Input
+        double P1X = Convert.ToDouble(Console.ReadLine()); 
+        double P1Y = Convert.ToDouble(Console.ReadLine()); 
+        double P2X = Convert.ToDouble(Console.ReadLine()); 
+        double P2Y = Convert.ToDouble(Console.ReadLine()); 
+        double P3X = Convert.ToDouble(Console.ReadLine()); 
+        double P3Y = Convert.ToDouble(Console.ReadLine());
+
+        // Calculate_Center_Of_Circle
+        double Cx = ((P2X - P1Y) * (P3Y * P3Y - P1Y * P1Y + P3X * P3X - P1X * P1X) + (P3Y - P1Y) * (P1Y * P1Y - P2Y * P2Y + P1X * P1X - P2X * P2X)) / (2 * (P3X - P1X) * (P2Y - P1Y) - 2 * (P2X - P1X) * (P3Y - P1Y));
+        double Cy = ((P2X - P1X) * (P3X * P3X - P1X * P1X + P3Y * P3Y - P1Y * P1Y) + (P3X - P1X) * (P1X * P1X - P2X * P2X + P1Y * P1Y - P2Y * P2Y)) / (2 * (P3Y - P1Y) * (P2X - P1X) - 2 * (P2Y - P1Y) * (P3X - P1X));
+
+        //Calculate_Radius
+        double Radius = Math.Sqrt((P1X - Cx) * (P1X - Cx) + (P1Y - Cy) * (P1Y - Cy));
+
+        // Output
+        Console.WriteLine(Cx);
+        Console.WriteLine(Cy);
+        Console.WriteLine(Radius);
+    }
+}
---
+++ No3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Input_Point
+        double C1x = Convert.ToDouble(Console.ReadLine());
+        double C1y = Convert.ToDouble(Console.ReadLine());
+        double C2x = Convert.ToDouble(Console.ReadLine());
+        double C2y = Convert.ToDouble(Console.ReadLine());
+
+        //Input_Radius
+        double radiusC1 = Convert.ToDouble(Console.ReadLine());
+        double radiusC2 = Convert.ToDouble(Console.ReadLine());
+
+        //Calculate_Distance_Between_C1_n_C2
+        double x = C2x - C1x;
+        double y = C2y - C1y;
+        double distance = Math.Sqrt((x * x) + (y * y));
+
+        if (distance <= radiusC1 + radiusC2) //Check_If_Intersect
+        {
+            double a = ((radiusC1 * radiusC1) - (radiusC2 * radiusC2) + (distance * distance)) / (2 * distance);
+            double h = Math.Sqrt((radiusC1 * radiusC1) - (a * a));
+            double midPointX = C1x + (a * (C2x - C1x)) / distance;
+            double midPointY = C1y + (a * (C2y - C1y)) / distance;
+            double VectorX = (h * (C2y - C1y)) / distance;
+            double VectorY = (h * (C2x - C1x)) / distance;
+
+            if (distance == radiusC1 + radiusC2) //Check_If_Connect
+            {
+                Console.WriteLine("{0:0.00}", midPointX - VectorX);
+                Console.WriteLine("{0:0.00}", midPointY + VectorY);
+            }
+
+            else //If_Not_Connect
+            {
+                Console.WriteLine("{0:0.00}", midPointX - VectorX);
+                Console.WriteLine("{0:0.00}", midPointY + VectorY);
+                Console.WriteLine("{0:0.00}", midPointX + VectorX);
+                Console.WriteLine("{0:0.00}", midPointY - VectorY);
+            }
+        }
+    }
+}
